bfd3ffe8d71bacd4983d397322a8ce42d6edc3c1
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2020-05-26 04:21:54 +0900

    first commit

---
+++ serverless/src/app.ts
@@ -0,0 +1,23 @@
+import 'source-map-support/register';
+
+import { APIGatewayEvent, APIGatewayProxyHandler, Context } from 'aws-lambda';
+import * as awsServerlessExpress from 'aws-serverless-express';
+import * as express from 'express';
+import { setupFireStore, setupFireStorage } from './common/firebase';
+
+const app = express();
+const server = awsServerlessExpress.createServer(app);
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+app.use(cors({ origin: true }));
+
+app.get('/', (req, res) => {
+  res.json({ hello: 'world' });
+});
+
+export const handler: APIGatewayProxyHandler = (event: APIGatewayEvent, context: Context) => {
+  awsServerlessExpress.proxy(server, event, context);
+};
